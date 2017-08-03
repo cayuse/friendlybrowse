ca09377b6acac64837a9b0adb88734a575538752
David Henderson <=>
2017-08-03 08:45:30 -0700

    minor changes

--- routes/browse.js
+++ routes/browse.js
@@ -15,7 +15,26 @@ router.get('/:site', function(req, res, next) {
 
 router.get('/', function(req, res, next) {
     var request = require("request");
-    request("http://www.sitepoint.com", function(error, response, body) {
+    request("http://www.example.com", function(error, response, body) {
+        var _write = res.write,
+            _writeHead = res.writeHead,
+            isHtml = false;
+        var scriptElm = "\n<script type='text/javascript' src='https://code.jquery.com/jquery-1.12.4.js'></script>\n";
+        //req.headers['accept-encoding'] = '*;q=1,gzip=0';
+        response.writeHead = function(code, headers) {
+            isHtml = headers['content-type'] && headers['content-type'].match('text/html');
+            if (isHtml) {
+                headers['content-length'] = parseInt(headers['content-length']) + scriptElm.length;
+            }
+            _writeHead.apply(this, arguments);
+        };
+
+        response.write = function(chunk) {
+            if (isHtml) {
+                chunk = chunk.toString().replace(/(<head[^>]*>)/, "$1" + scriptElm)
+            }
+            _write.call(res, chunk);
+        };
         res.send(body);
 //        res.render('browse', {data: body});
   //      console.log(body);
