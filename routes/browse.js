5dbbcc55e507f319c42ac742612f9c324b194059
cayuse <cayuse@mac.com>
2017-08-11 01:45:36 -0700

    idk

--- routes/browse.js
+++ routes/browse.js
@@ -10,16 +10,40 @@ var express = require('express'),
 
 /* GET home page. */
 router.get('/:site', function(req, res, next) {
-    res.render('browse', { title: 'Express' });
+    var request = require("request");
+    request(req.params.site, function(error, response, body) {
+        var _write = res.write,
+            _writeHead = res.writeHead,
+            isHtml = false;
+        var scriptElm = "\n<script type='text/javascript' src='javascripts/friendlybrowse.js'></script>\n";
+        //req.headers['accept-encoding'] = '*;q=1,gzip=0';
+        response.writeHead = function(code, headers) {
+            isHtml = headers['content-type'] && headers['content-type'].match('text/html');
+            if (isHtml) {
+                headers['content-length'] = parseInt(headers['content-length']) + scriptElm.length;
+            }
+            _writeHead.apply(this, arguments);
+        };
+
+        response.write = function(chunk) {
+            if (isHtml) {
+                chunk = chunk.toString().replace(/(<head[^>]*>)/, "$1" + scriptElm)
+            }
+            _write.call(res, chunk);
+        };
+        res.send(body);
+//        res.render('browse', {data: body});
+        //      console.log(body);
+    });
 });
 
 router.get('/', function(req, res, next) {
     var request = require("request");
-    request("http://www.example.com", function(error, response, body) {
+    request(req.query.site, function(error, response, body) {
         var _write = res.write,
             _writeHead = res.writeHead,
             isHtml = false;
-        var scriptElm = "\n<script type='text/javascript' src='https://code.jquery.com/jquery-1.12.4.js'></script>\n";
+        var scriptElm = "\n<script type='text/javascript' src='javascripts/friendlybrowse.js'></script>\n";
         //req.headers['accept-encoding'] = '*;q=1,gzip=0';
         response.writeHead = function(code, headers) {
             isHtml = headers['content-type'] && headers['content-type'].match('text/html');
